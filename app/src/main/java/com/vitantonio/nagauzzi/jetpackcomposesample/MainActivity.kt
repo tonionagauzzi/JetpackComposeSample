396740b8e7569fa38b449a6d9cd8dcc5c59394ce
Kensaku Nagauchi <kensaku.nagauchi@access-company.com>
2020-04-30 16:33:40 +0900

    temporary commit

--- app/src/main/java/com/vitantonio/nagauzzi/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/vitantonio/nagauzzi/jetpackcomposesample/MainActivity.kt
@@ -3,19 +3,41 @@ package com.vitantonio.nagauzzi.jetpackcomposesample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
+import androidx.ui.core.Clip
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
+import androidx.ui.foundation.DrawImage
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.layout.*
+import androidx.ui.res.imageResource
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Tonio")
+            prize()
         }
     }
 
+    @Preview
     @Composable
-    fun Greeting(name: String) {
-        Text(text = "Hello $name!")
+    fun prize() {
+        val image = +imageResource(R.drawable.gtr)
+        Column(
+            modifier = Spacing(16.dp)
+        ) {
+            Container(height = 180.dp) {
+                Clip(shape = RoundedCornerShape(8.dp)) {
+                    DrawImage(image)
+                }
+            }
+            HeightSpacer(16.dp)
+            Text("A day in Shark Fin Cove")
+            Text("Davenport, California")
+            Text("December 2018")
+        }
     }
 }
